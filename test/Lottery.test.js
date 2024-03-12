6f1a153e3ad478e2dfe597c56f21d98e63bc5562
Ndhlovu1 <tinomudaishendhlovu@gmail.com>
2024-03-12 20:12:40 +0200

    Updated : Tests for entering the lottery draw

--- test/Lottery.test.js
+++ test/Lottery.test.js
@@ -23,5 +23,53 @@ describe('Lottery Contract', () => {
     it('deploys a contract', () => {
         assert.ok(lottery.options.address)
     })
+
+    it('One Account', async () => {
+
+        await lottery.methods.enter().send({
+            from : accounts[0],
+            value : web3.utils.toWei('0.02', 'ether')
+        })
+
+         //Attempt to call player methoid to get the list of players
+        const players = await lottery.methods.getPlayers().call({
+            from: accounts[0]
+        })
+
+        //Ensure
+        assert.equal(accounts[0], players[0])
+        assert.equal(1, players.length)
+
+    })
+
+    it('Multiple Accounts', async () => {
+
+        await lottery.methods.enter().send({
+            from : accounts[0],
+            value : web3.utils.toWei('0.02', 'ether')
+        })
+
+        await lottery.methods.enter().send({
+            from : accounts[1],
+            value : web3.utils.toWei('0.02', 'ether')
+        })
+
+        await lottery.methods.enter().send({
+            from : accounts[2],
+            value : web3.utils.toWei('0.02', 'ether')
+        })
+
+        const players = await lottery.methods.getPlayers().call({
+            from : accounts[0]
+        })
+
+        assert.equal(accounts[0], players[0])
+        assert.equal(accounts[1], players[1])
+        assert.equal(accounts[2], players[2])
+
+
+    })
+
+   
 })
 
