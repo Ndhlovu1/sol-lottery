f503cbb5e539999a366289a66258614a528ebb71
Ndhlovu1 <tinomudaishendhlovu@gmail.com>
2024-03-12 19:44:33 +0200

    Added Lottery.test.js to perform local tests with Mocha for our contract

---
+++ test/Lottery.test.js
@@ -0,0 +1,27 @@
+const ganache = require('ganache');
+const { Web3 } = require('web3');
+// updated imports added for convenience
+const assert = require('assert');
+const web3 = new Web3(ganache.provider())
+
+const { interface, bytecode } = require('../compile');
+
+let lottery;
+let accounts;
+
+beforeEach(async () => {
+
+    accounts = await web3.eth.getAccounts();
+
+    lottery = await new web3.eth.Contract(JSON.parse(interface))
+        .deploy({ data: bytecode })
+        .send({ from: accounts[0], gas: '1000000' })
+});
+
+describe('Lottery Contract', () => {
+
+    it('deploys a contract', () => {
+        assert.ok(lottery.options.address)
+    })
+})
+
