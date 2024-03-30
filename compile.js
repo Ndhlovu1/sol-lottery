329ec7e47f3b4a64a15c34d000182019c836d51f
Ndhlovu1 <tinomudaishendhlovu@gmail.com>
2024-03-30 17:23:39 +0200

    Updated : Compile.js file to show successful message

--- compile.js
+++ compile.js
@@ -5,4 +5,6 @@ const solc = require("solc");
 const lotteryPath = path.resolve(__dirname, "contracts", "Lottery.sol");
 const source = fs.readFileSync(lotteryPath, "utf8");
 
+console.log("--SUCCESSFULLY COMPILED--")
+
 module.exports = solc.compile(source, 1).contracts[":Lottery"];
