f5b92fb34ac71ff32d14073ecbfb3b1b3816687c
Ndhlovu1 <tinomudaishendhlovu@gmail.com>
2024-05-05 01:04:41 +0200

    Added : Get Winning Player Contract

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -3,32 +3,38 @@ pragma solidity ^0.4.17;
 contract Lottery {
     address public manager;
     address[] public players;
+    address public winnerPlayer;
     
     function Lottery() public {
         manager = msg.sender;
     }
-    
+
     function enter() public payable {
-        require(msg.value > .01 ether);
+        require(msg.value > .0001 ether);
         players.push(msg.sender);
     }
-    
+
     function random() private view returns (uint) {
         return uint(keccak256(block.difficulty, now, players));
     }
-    
+
     function pickWinner() public restricted {
         uint index = random() % players.length;
         players[index].transfer(this.balance);
+        winnerPlayer = players[index];
         players = new address[](0);
     }
-    
+
+    function getWinner() public view returns (address) {
+        return  winnerPlayer;
+    }
+
     modifier restricted() {
         require(msg.sender == manager);
         _;
     }
-    
+
     function getPlayers() public view returns (address[]) {
         return players;
     }
-}   
+}
